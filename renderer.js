c57ac8f5989b06280d8d7ceb66b3cad547f0b347
Boris Kreminski <krembf@gmail.com>
2019-05-04 01:27:15 -0400

    Added addon based on node-addon-api

--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+console.log("Renderer Enter");
+
+var addon = require('bindings')('hello.node');
+
+document.body.onload = addElement;
+
+function addElement () { 
+  // create a new div element 
+  var newDiv = document.createElement("div"); 
+  // and give it some content 
+  var newContent = document.createTextNode(addon.hello()); 
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);  
+
+  // add the newly created element and its content into the DOM 
+  var currentDiv = document.getElementById("div1"); 
+  document.body.insertBefore(newDiv, currentDiv); 
+}
